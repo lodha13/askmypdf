eeb1c461f6184b65e52a4d59521f3e6d05323831
Abhishek Lodha <abhishek_lodha13@yahoo.co.in>
2024-12-31 18:13:21 +0530

    auth provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,40 @@
-import Navbar from '@/components/Navbar'
-import { cn, constructMetadata } from '@/lib/utils'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import Navbar from "@/components/Navbar";
+import { cn, constructMetadata } from "@/lib/utils";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-import 'react-loading-skeleton/dist/skeleton.css'
-import 'simplebar-react/dist/simplebar.min.css'
+import "react-loading-skeleton/dist/skeleton.css";
+import "simplebar-react/dist/simplebar.min.css";
 
-import { Toaster } from '@/components/ui/toaster'
-import Providers from '@/components/Provider'
+import { Toaster } from "@/components/ui/toaster";
+import Providers from "@/components/Provider";
+import { AuthProvider } from "./AuthProvider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = constructMetadata()
+export const metadata = constructMetadata();
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang='en' className='light'>
-      <Providers>
-        <body
-          className={cn(
-            'min-h-screen font-sans antialiased grainy',
-            inter.className
-          )}>
-          <Toaster />
-          <Navbar />
-          {children}
-        </body>
-      </Providers>
-    </html>
-  )
+    <AuthProvider>
+      <html lang="en" className="light">
+        <Providers>
+          <body
+            className={cn(
+              "min-h-screen font-sans antialiased grainy",
+              inter.className
+            )}
+          >
+            <Toaster />
+            <Navbar />
+            {children}
+          </body>
+        </Providers>
+      </html>
+    </AuthProvider>
+  );
 }
